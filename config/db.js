5b9c3879b6e3b16c1ab3b99a5ab36dd5c8d2194d
Kuteji <edutalavera@outlook.com>
2020-04-03 01:49:06 -0600

    Instalando Mongoose y conectando la base de datos

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require('dotenv').config({path: 'variables.env'});
+
+mongoose.connect(process.env.DATABASE,{
+    useUnifiedTopology: true,
+    useNewUrlParser:true 
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log(error);
+})
